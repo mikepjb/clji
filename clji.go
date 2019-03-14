27f37e41372dbb10d03c84a26a06c9e77c1aac21
Michael Bruce <me@michaelbruce.co>
2019-03-14 00:17:25 +0000

    read optional port; trouble reading delimiting output from nREPL

--- clji.go
+++ clji.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"os"
 
 	"github.com/mikepjb/clji/src/bencode"
 )
 
 func main() {
+	port := "9999"
+
+	if len(os.Args) != 1 {
+		port = os.Args[1]
+	}
+
 	clone := map[string]string{"op": "clone"}
 	emsg := bencode.Encode(clone)
 	fmt.Println(emsg)
+
+	conn, _ := net.Dial("tcp", "127.0.0.1:"+port)
+	fmt.Fprintf(conn, emsg+"\n")
+
+	// b, _ := bufio.NewReader(conn).ReadLine()
+	// fmt.Print("response: " + string(b))
 }
